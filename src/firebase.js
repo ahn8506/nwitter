2eec754a254d1f723b7c7d9036153557a28526b2
jong hyun ahn <ahn8506@gmail.com>
2021-09-12 20:16:44 +0900

    1.0 First

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvyCrNKrGN4iOYnsxod-UGss5a2ep_bms",
+    authDomain: "nwitter-ab1f0.firebaseapp.com",
+    projectId: "nwitter-ab1f0",
+    storageBucket: "nwitter-ab1f0.appspot.com",
+    messagingSenderId: "708414183217",
+    appId: "1:708414183217:web:d701bce57f50cb9f524210"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
